a6130545d1a968ea273cec8ef64b1747bd7b0027
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2024-01-04 20:01:01 +0300

    adding hardware

---
+++ main/main.ino
@@ -0,0 +1,17 @@
+/**
+code to record patterns
+follow patterns
+
+
+**/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
